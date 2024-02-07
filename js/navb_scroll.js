1e990cfc7169cd634323ee5a0f57b268205fbabc
MikeD82 <michal.drazek@gmail.com>
2024-02-07 15:58:38 +0100

    wgrane pliki do glownego katalogu w celu testowania w realu

---
+++ js/navb_scroll.js
@@ -0,0 +1,20 @@
+﻿var prevScrollpos = window.scrollY;
+var ticking = false;
+
+window.onscroll = function () {
+    if (!ticking) {
+        window.requestAnimationFrame(function () {
+            var currentScrollPos = window.scrollY;
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("navbar").style.top = "0";
+            } else {
+                document.getElementById("navbar").style.top = "-60px";
+            }
+            prevScrollpos = currentScrollPos;
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+};
+
