1a63dd61c2994f8c4c709efc65d3a598ac98a92b
MikeD82 <michal.drazek@gmail.com>
2024-02-08 20:20:24 +0100

    under construction wgrane do głównego katalogu
    usunięto rownież z kodu animację zwiazaną z sekcją #about

--- js/copyright_paragraph.js
+++
@@ -1,5 +0,0 @@
-var currentDate = new Date();
-        var currentYear = currentDate.getFullYear();
-        
-        var paragraphText = "&copy; " + currentYear + " MDRS. All rights reserved.";
-        document.getElementById("copyrightParagraph").innerHTML = paragraphText; 
