1272a92e8bbfd3b260fa19c2045b327005ee447d
MikeD82 <michal.drazek@gmail.com>
2024-02-09 17:29:46 +0100

    construction works again on server

--- js/scrollToAbout.js
+++
@@ -1,8 +0,0 @@
-function scrollToAbout() {
-    
-    // Przewiń do sekcji "#About"    
-    var aboutSection = document.getElementById('About');
-    aboutSection.scrollIntoView({ behavior: 'smooth' });
-    document.getElementById("navbar").style.top = "-60px";
-    
-}
