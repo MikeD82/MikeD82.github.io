65d6454f0e531402a279eda58e0bcf4eeab4b0e6
MikeD82 <michal.drazek@gmail.com>
2024-02-12 14:47:46 +0100

    testowe wgranie na serwer

---
+++ js/animated_element.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener("scroll", function () {
+    var animatedElements = document.querySelectorAll(".animated-element");
+
+    for (var i = 0; i < animatedElements.length; i++) {
+        var element = animatedElements[i];
+        var elementPosition = element.getBoundingClientRect().top;
+        var windowHeight = window.innerHeight;
+
+        if (elementPosition < windowHeight) {
+            element.classList.add("animated");
+        }
+    }
+});
