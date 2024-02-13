761409131259b519260d879d83de9517d2f18b9a
MikeD82 <michal.drazek@gmail.com>
2024-02-13 17:32:05 +0100

    kolejne testowe załadowanie na serwer

--- MDRS Files/js/toggleMenuAndScrollToAbout.js
+++ MDRS Files/js/toggleMenuAndScrollToAbout.js
@@ -7,9 +7,34 @@ function toggleMenuAndScrollToAbout() {
         checkbox.checked = true; // Otwórz menu
         document.body.classList.add('lock-scroll'); // Dodaj klasę blokującą przewijanie
     }
-    // Przewiń do sekcji "#About"
-    var aboutSection = document.getElementById('About');
-    aboutSection.scrollIntoView({ behavior: 'smooth' });
-    checkbox.checked = false;    
-    document.getElementById("navbar").style.top = "-60px";
+    
+    // Sprawdź szerokość ekranu
+    if (window.innerWidth < 576) {
+    
+        if (window.location.href =='index.html')
+        {
+          // Przewiń do sekcji "#About" tylko na ekranach o szerokości mniejszej niż 576 pikseli
+           var aboutSection = document.getElementById('About');
+           aboutSection.scrollIntoView({ behavior: 'smooth' });
+           checkbox.checked = false;    
+           document.getElementById("navbar").style.top = "-60px";
+        
+        } 
+        else
+        {
+       // Define the URL of the page you want to navigate to
+          var targetPageUrl = 'index.html';
+       // Define the section identifier you want to navigate to
+          var sectionId = 'About';
+        // Combine the URL with the section identifier as a hash
+        var targetUrlWithHash = targetPageUrl + '#' + sectionId;
+
+          // Navigate to the target URL
+             window.location.href = targetUrlWithHash;
+             //checkbox.checked = false;    
+             //document.getElementById("navbar").style.top = "-60px";
+
+        }
+    }  
 }
+
